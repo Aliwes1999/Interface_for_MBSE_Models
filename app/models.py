c5d88028d983925f411a05ea1a3df0d6937d5611
Aliwes1999 <166415247+Aliwes1999@users.noreply.github.com>
2025-12-02 18:31:52 +0100

    Increase email field length to 256 characters

--- app/models.py
+++ app/models.py
@@ -17,7 +17,7 @@ project_user_association = db.Table('project_user_association',
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(128), unique=True, nullable=False)
+    email = db.Column(db.String(256), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
